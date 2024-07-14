f99647b8018d9b63567613dd5663ce3b71a28a00
Gustavo Mello Tonnera <gmtonnera@gmail.com>
2024-07-14 16:42:56 -0300

    aula 2

---
+++ planner/src/app/trip/activities.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import { TripData } from "./[id]";
+
+type Props = {
+  tripDetails: TripData;
+};
+
+export function Activities({ tripDetails }: Props) {
+  return <View className="flex-1"></View>;
+}
